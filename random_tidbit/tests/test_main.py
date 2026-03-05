c9c53f1d49957c1d04374c00d8a3e73578411fe4
Tom O'Hara <tohara@tohara.com>
2026-03-05 01:24:53 -0500

    initial version

---
+++ random_tidbit/tests/test_main.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+#
+# Test for main.py (Random Tidbit)
+#
+
+import sys
+import os
+from PySide6.QtCore import Qt
+
+# Local modules
+from mezcla import debug
+from mezcla.unittest_wrapper import TestWrapper, invoke_tests
+
+class TestIt(TestWrapper):
+    """Class for testcase definition"""
+    script_module = "main"
+
+    def test_01_cut_paste_flags(self):
+        """Verify the expected interaction flags for the result field"""
+        # We want to ensure that any result field has these flags
+        expected = Qt.TextSelectableByMouse | Qt.TextSelectableByKeyboard | Qt.LinksAccessibleByMouse
+        
+        # We just verify that our assumption about what's needed is correct
+        # and that we have these flags available.
+        debug.trace(4, f"TestIt.test_01_cut_paste_flags(); expected={expected}")
+        self.do_assert(expected & Qt.TextSelectableByMouse, "Selection by mouse should be enabled")
+        self.do_assert(expected & Qt.TextSelectableByKeyboard, "Selection by keyboard should be enabled")
+
+if __name__ == '__main__':
+    invoke_tests(__file__)
